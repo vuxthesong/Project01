d4291199399c1fab6a8a42253d8e98ee63822973
vuxthesong <vusong25@gmail.com>
2022-11-16 22:54:18 +0700

    create table sql

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -1,12 +1,19 @@
 "use strict";
 
+const { DATE } = require("sequelize");
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Users", [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "example@example.com",
+        email: "admin@gmail.com",
+        password: "123456",
+        firstName: "Vu The",
+        lastName: "Song",
+        address: "Ha Noi",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
